37b162f10c31191f9430dcd6236328d7f2394ff8
hezhuozhuo <421045249@qq.com>
2024-03-12 21:17:34 +0800

    fix bug

--- plugin/thingspanel/mqtt.go
+++ plugin/thingspanel/mqtt.go
@@ -55,27 +55,27 @@ func (c *MqttClient) SendData(topic string, data []byte) error {
 			return
 		}
 	}()
-	go func() {
-		Log.Info("检查MqttClIent连接状态...")
-		if !c.IsFlag {
-			i := 1
-			for {
-				fmt.Println("等待...", i)
-				if i == 10 || c.IsFlag {
-					break
-				}
-				time.Sleep(1 * time.Second)
-				i++
+	//go func() {
+	Log.Info("检查MqttClIent连接状态...")
+	if !c.IsFlag {
+		i := 1
+		for {
+			fmt.Println("等待...", i)
+			if i == 10 || c.IsFlag {
+				break
 			}
+			time.Sleep(1 * time.Second)
+			i++
 		}
-		Log.Info("发送设备状态...")
-		token := c.Client.Publish(topic, 1, false, string(data))
-		if !token.WaitTimeout(5 * time.Second) {
-			Log.Warn("发送设备状态超时")
-		} else if err := token.Error(); err != nil {
-			Log.Warn("发送设备状态失败: " + err.Error())
-		}
-		Log.Info("发送设备状态完成")
-	}()
+	}
+	Log.Info("发送设备状态...")
+	token := c.Client.Publish(topic, 0, false, string(data))
+	if !token.WaitTimeout(5 * time.Second) {
+		Log.Warn("发送设备状态超时")
+	} else if err := token.Error(); err != nil {
+		Log.Warn("发送设备状态失败: " + err.Error())
+	}
+	Log.Info("发送设备状态完成")
+	//}()
 	return nil
 }
